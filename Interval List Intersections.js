25afac8869bb1ff591d0246bdf2f3c2aa2a24b39
Balaji Saravanan <balajilitsv@gmail.com>
2020-05-23 23:09:05 +0530

    Interval List Intersections
    Given two lists of closed intervals, each list of intervals is pairwise disjoint and in sorted order.
    
    Return the intersection of these two interval lists.
    
    (Formally, a closed interval [a, b] (with a <= b) denotes the set of real numbers x with a <= x <= b.  The intersection of two closed intervals is a set of real numbers that is either empty, or can be represented as a closed interval.  For example, the intersection of [1, 3] and [2, 4] is [2, 3].)
    
     
    
    Example 1:
    
    Input: A = [[0,2],[5,10],[13,23],[24,25]], B = [[1,5],[8,12],[15,24],[25,26]]
    Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
    Reminder: The inputs and the desired output are lists of Interval objects, and not arrays or lists.
    
     
    
    Note:
    
        0 <= A.length < 1000
        0 <= B.length < 1000
        0 <= A[i].start, A[i].end, B[i].start, B[i].end < 10^9
    
    NOTE: input types have been changed on April 15, 2019. Please reset to default code definition to get new method signature.

--- Interval List Intersections.js
+++ Interval List Intersections.js
@@ -3,6 +3,7 @@
  * @param {number[][]} B
  * @return {number[][]}
  */
+// Runtime: 136ms for the testcases in leetcode
 var intervalIntersection = function(A, B) {
 	let closedInterval = [];
 	for(let i = 0; i < A.length; i++) {
@@ -15,5 +16,29 @@ var intervalIntersection = function(A, B) {
 					}
 			}
 	}
+	return closedInterval;
+};
+
+
+/**
+ * @param {number[][]} A
+ * @param {number[][]} B
+ * @return {number[][]}
+ */
+// Runtime: 94ms for the testcases in leetcode
+var intervalIntersection = function(A, B) {
+	let closedInterval = [], i = 0, j = 0;
+	while(i < A.length && j < B.length) {
+        let start = Math.max(A[i][0], B[j][0]);
+        let end = Math.min(A[i][1], B[j][1]);
+        if(start <= end) {
+           closedInterval.push([start, end]); 
+        }
+        if(A[i][1] < B[j][1]) {
+            i++;
+        } else {
+            j++;
+        }
+    }
 	return closedInterval;
 };
